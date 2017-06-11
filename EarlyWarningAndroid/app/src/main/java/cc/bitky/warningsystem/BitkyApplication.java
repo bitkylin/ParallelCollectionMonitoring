5dba92674443d95d3bf078887a110fee8266df35
bitkylin <limingliang2004@126.com>
2017-06-11 13:25:19 +0800

    添加 BitkyApplication.java，去除隐私信息，项目完整开源

---
+++ EarlyWarningAndroid/app/src/main/java/cc/bitky/warningsystem/BitkyApplication.java
@@ -0,0 +1,20 @@
+package cc.bitky.warningsystem;
+
+import android.app.Application;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+import cn.bmob.v3.Bmob;
+
+public class BitkyApplication extends Application {
+  final String Bmob_application_ID = "你的 Bmob Key";
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SDKInitializer.initialize(this);
+    Fresco.initialize(this);
+    Bmob.initialize(this, Bmob_application_ID);
+  }
+}
